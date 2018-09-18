cfddb59cf704fead7bda5892ca3abf7887747592
Austin Peterson <austintpeterson@gmail.com>
2018-09-17 17:21:16 -0700

    added my pathway to pi script

--- jsonToSql/JsonToSql_cu_gsr.py
+++ jsonToSql/JsonToSql_cu_gsr.py
@@ -1,11 +1,19 @@
-#designed to convert all the json CU warning data provided by IARPA (in the cu_gsr data)
+#designed to convert all the json CU event data provided by IARPA (in the cu_gsr data)
 #into a sql database for use later.  will try to format dates into 
 #sql datetime format for easy time-based queries.
+
+#hopefully all cu event warning will be inserted into the same db
 
 #created by Austin Peterson
 
 import json
 import sqlite3
+import time
 
-with open("__", "r") as read_file:
+# need time.strftime to conv json time to sql datetime
+#https://stackoverflow.com/questions/6119369/simple-datetime-sql-query
+
+with open("/Users/austinpeterson/Documents/code/MercuryChallenge/jsonToSql/cu_gsr/CU_April_2016.json", "r") as read_file:
     data = json.load(read_file)
+
+    print(data)
