f69735f5fe40f04f2c2648c2bb1764472c376892
Austin Peterson <austintpeterson@gmail.com>
2018-09-17 18:55:13 -0700

    added first bit of json parsing

--- jsonToSql/JsonToSql_cu_gsr.py
+++ jsonToSql/JsonToSql_cu_gsr.py
@@ -9,11 +9,76 @@
 import json
 import sqlite3
 import time
+import os
 
 # need time.strftime to conv json time to sql datetime
 #https://stackoverflow.com/questions/6119369/simple-datetime-sql-query
 
-with open("/Users/austinpeterson/Documents/code/MercuryChallenge/jsonToSql/cu_gsr/CU_April_2016.json", "r") as read_file:
-    data = json.load(read_file)
 
-    print(data)
+
+
+def main():
+
+	path = '/Users/austinpeterson/Documents/code/MercuryChallenge/jsonToSql/cu_gsr/'
+
+	#read each json file included in cu_gsr
+	for filename in os.listdir(path):
+
+		temppath = path+filename
+
+		with open(temppath, "r") as read_file:
+			data = json.load(read_file)
+
+		#print(data)
+
+		#json fields
+
+		#Approximate_Location (str)
+		#City (str)
+		#Country (str)
+		#Crowd_Size (str)
+		#Crowd_Size_Description (str)
+		#Earliest_Reported_Date (put in datetime format)
+		#Encoding_Comment (str)
+		#Event_Date (put in datetime format)
+		#Event_ID (str)
+		#Event_Type (str)
+		#First_Reported_Link (str)
+		#GSS_Link (str)
+		#Latitude (float/)
+		#Longitude (float/)
+		#News_Source (str)
+		#Other_Links (str, w/ newlines separating)
+		#Population (str)
+		#Reason (str)
+		#Revision_Date (put in datetime format)
+		#State (str)
+		#Revision_DTG (int?)
+
+		#parse json data into vars
+		for i in data:
+			print(i['City'])
+
+			try:
+				Approximate_Location = i['Approximate_Location']
+				print(Approximate_Location)
+				
+			except Error as e:
+				print(e)
+
+
+
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+	
