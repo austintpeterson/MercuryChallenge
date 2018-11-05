d36d4d2e24d23900731cd16623688275ab9aabb1
Austin Peterson <austintpeterson@gmail.com>
2018-11-05 12:35:04 -0700

    practice stuff added

--- twitter/scraper1.py
+++ twitter/scraper1.py
@@ -1,4 +1,24 @@
-import twitterscraper
+#https://github.com/taspinar/twitterscraper/
 
-#can be used as a command line tool as well,
-#ex: twitterscraper realDonaldTrump -u -o tweets_username.json
+from twitterscraper import query_tweets
+import datetime as dt
+
+#can be used as a command line tool as well, very useful
+#ex: twitterscraper realDonaldTrump -u -o tweets_trump.json
+
+#can get from bd (begindate) to ed (enddate)
+#twitterscraper Trump -l 100 -bd 2017-01-01 -ed 2017-06-01 -o trumptweets.json/.txt
+
+#Per Tweet it scrapes the following information: + Username and Full Name + Tweet-id + Tweet-url + Tweet text + Tweet html + Tweet timestamp + No. of likes + No. of replies + No. of retweets
+#Each tweet has these attribs: user, fullname, id, url, timestamp, text, replies, rewteets, likes, html
+
+#query_tweets(query, limit=None, begindate=dt.date(2006,3,21), enddate=dt.date.today(), poolsize=20, lang='')
+
+list_of_tweets = query_tweets("Muslim Brotherhood", 10)
+
+#can write to file
+file = open("output.txt","w")
+for tweet in query_tweets("Egypt", 10):
+	file.write(tweet.text)#write text of tweet to file
+	file.write("\n\n")
+file.close()
