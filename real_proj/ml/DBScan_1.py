2eacd74ae78fdf11addfe2fee914644936f4881e
Austin Peterson <austintpeterson@gmail.com>
2019-02-13 18:37:09 -0700

    using tfidfVctrzr to transform text data, fitting to DBSCAN

--- real_proj/ml/DBScan_1.py
+++ real_proj/ml/DBScan_1.py
@@ -2,9 +2,14 @@
 #file for first try at using DBScan on collected Arabic Tweets
 
 from sklearn.cluster import DBSCAN
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 import pandas as pd
 from leven import levenshtein
 import numpy as np
+
+#for plotting/graphics
+import matplotlib.pyplot as plt
 
 import os.path
 import sys
@@ -22,7 +27,8 @@ import loader
 
 
 def main():
-	target_xlsx = input("target xlsx (without file extension): ")
+	#target_xlsx = input("target xlsx (without file extension): ")
+	target_xlsx = "outputSmall"
 
 	#todo: convert to list of strings ('text' column)
 	data_list = loader.load_tweets_xlsx(target_xlsx)
@@ -34,25 +40,50 @@ def main():
 	#train_seg = .8
 	#test_seg = 1-float(train_seg)
 
+	#vectorizer(tfidf)
+	#using vectorizer to transform text
+	vectorizer = TfidfVectorizer()
+	X = vectorizer.fit_transform(text_list)
+	
+
+
 	#can define custom distance metrics for DBScan
 	#check it out
 	#https://scikit-learn.org/stable/faq.html#how-do-i-deal-with-string-data-or-trees-graphs
 	#levenshtein is good for strings
+	#NOT CURRENTLY USED
 	def lev_metric(x, y):
 		i, j = int(x[0]), int(y[0])#extract indices
 		return levenshtein(data_list[i], data_list[j])
 
 
 	#create dbscan
-	X = np.arange(len(data_list)).reshape(-1, 1)
-	db = dbscan(X, metric=lev_metric, eps=5, min_samples=2)
+	#X = np.arange(len(data_list)).reshape(-1, 1)
+	#db = dbscan(X, metric=lev_metric, eps=5, min_samples=2)
+
+	#this is where we need to play around with parameters
+	db = DBSCAN(eps = 0.3, min_samples = 5)#don't wanna go lower than 5
 
 	#fit data to dbscan
-	db.fit(data_list)
-
-	print(db)
+	db.fit(X)
 
 
+
+	#plotting
+	labels = db.labels_
+
+	#number of clusters (ignoring noise)
+	n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+
+	#with outputSmall test data, clusters coming out to 23
+	print("# of labels(clusters): "+str(n_clusters))
+
+
+
+
+
+
+main()
 
 
 
