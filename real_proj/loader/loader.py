18ab46d41966ea93ea49ea0bc1c78c11ab58f551
Austin Peterson <austintpeterson@gmail.com>
2019-02-13 17:42:46 -0700

    loader now successfully loads target files from tweet_collections (in Preston's format)

--- real_proj/loader/loader.py
+++ real_proj/loader/loader.py
@@ -1,19 +1,16 @@
 
 #Austin P
-#
-
-#from xlrd import open_workbook
-#import pyexcel
+#this is for loading xlsx from the tweet_collections dir in /scraping/
 
 import pandas as pd
+import numpy as np
 import csv
 import os.path
 import string
+import math
 
-#switch to xlsx file reading
-#https://pythonspot.com/read-xls-with-pandas/
-
-#for loading word list (word, count)
+#for loading keyword list csv (word, count)
+#this is currently UNUSED, ignore it
 def load_tweets(target_csv):
 	path = os.path.dirname(os.path.abspath(__file__))
 
@@ -48,27 +45,77 @@ def load_tweets(target_csv):
 
 
 
-def load_tweets(target_xlsx):
-	path = os.path.dirname(os.path.abspath(__file__))
+#function for loading tweet_collections xlsx into a pandas dataframe
+#can be modified later depending on format of data coming in
+def load_tweets_xlsx(target_xlsx):
+	
+	#get current path and parent path
+	my_path = os.path.dirname(os.path.abspath(__file__))
+	parent_path = os.path.abspath(os.path.join(my_path, os.pardir))
 
-	file_path = path+"/"+my_df
+	file_path = parent_path+"/scraping/tweet_collections/"+target_xlsx+".xlsx"
 
-	my_df = pd.read_excel(file_path, sheet_name=None)
+	print("xlsx path to load: "+file_path+"\n")
+	
+	#probably costly to load whole xlsx, but allows us to filter columns/rows later
+	my_df = pd.read_excel(file_path, sheet_name = "Sheet1")#(file_path, index_col = 0)
+
+	#print(my_df.columns)
+
+	#clean df here
+	cleaned_df = clean_loaded_df(my_df)
+
+	#print(cleaned_df['text'])
+	return cleaned_df
+	
 	
 
+#takes raw dataframe loaded by load_tweets_xlsx and cleans up/filters the columns needed
+#the filtering/cleaning can be changed later
+#takes a df as parameter, returns a 'clean' dataframe
+def clean_loaded_df(dirty_df):
+	#delete columns we don't need for ml
+	#these stmts can be added/commented based on what we want for our clean_df
+	
+	#1 for columns, 0 for rows
+	#'Number' column is kept for filtering keyword tag rows
+	new_df = dirty_df.drop('user',1)
+	new_df = new_df.drop('fullname',1)
+	new_df = new_df.drop('tweet-id',1)
+	new_df = new_df.drop('timestamp',1)
+	new_df = new_df.drop('url',1)
+	new_df = new_df.drop('likes',1)
+	new_df = new_df.drop('replies',1)
+	new_df = new_df.drop('retweets',1)
+	#ALWAYS keeping the 'text' column
+	new_df = new_df.drop('html',1)
+
+	#['Number', 'text']
+	#print(new_df.columns)
+	
+	#filter rows here
+	#getting rid of "keyword tag" rows that aren't tweet/data rows
+	#these rows have nothing in 'text' column ('')
+	
+	#note: there is probably a better way to do this, but I struggled with 
+	#issues regarding how NaN and int values are read in from xlsx for a while
+	#if xlsx format in changes, this will have to be addressed
+
+	#replace any empty text rows with numpy nan (this is supported by pandas)
+	new_df['text'].replace('', np.nan, inplace=True)
+	#drop any rows that have numpy nan in text column
+	new_df.dropna(subset = ['text'], inplace=True)
+
+	print(new_df.shape)
+
+	print(new_df["Number"])
+
+	return new_df
 
 
 
-
-#load tweets as they're being clustered so that you can look through
-#the tweets that were classified into each cluster
-#
-
-#do DBscan idiot
-#https://www.dummies.com/programming/big-data/data-science/how-to-create-an-unsupervised-learning-model-with-dbscan/
-
-
-
+#testing
+load_tweets_xlsx("outputSmall")
 
 
 
